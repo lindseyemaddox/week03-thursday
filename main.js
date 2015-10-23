566796514c79847964ff88bcd9e32bf5e275399e
lindseyemaddox <lindseyemaddox@gmail.com>
2015-10-23 10:39:11 -0500

    got all the operators working using number inputs

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
-   	var calculate = document.getElementById('calculateBtn');
+	var num_one = document.getElementById('num_one').value;
+	var num_two = document.getElementById('num_two').value;
 
-   	var handleKeyPress = function add() {
-    	var num_one = document.getElementById('num_one').value;
-    	var num_two = document.getElementById('num_two').value;
+   	var add = document.getElementById('additionBtn');
+   	var subtract = document.getElementById('subtractionBtn');
+   	var multiply = document.getElementById('multiplicationBtn');
+   	var divide = document.getElementById('divisionBtn');
+
+   	var addition = function calculateSum() {
 		var sum = parseInt(num_one) + parseInt(num_two);
     	document.getElementById('answer').innerHTML = sum;
   	}
 
-   calculate.addEventListener('click', handleKeyPress);
+   	var subtraction = function calculateProduct() {
+		var product = parseInt(num_one) - parseInt(num_two);
+    	document.getElementById('answer').innerHTML = product;
+  	}
+
+   	var multiplication = function calculateFactor() {
+		var factor = parseInt(num_one) * parseInt(num_two);
+    	document.getElementById('answer').innerHTML = factor;
+  	}
+
+   	var division = function calculateResult() {
+		var result = parseInt(num_one) / parseInt(num_two);
+    	document.getElementById('answer').innerHTML = result;
+  	}
+
+   	add.addEventListener('click', addition);
+   	subtract.addEventListener('click', subtraction);
+   	multiply.addEventListener('click', multiplication);
+   	divide.addEventListener('click', division);
